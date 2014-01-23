cab459c4a7e3b0440477e4e5be1cc6655072fbc3
Crimsoneer <andrea.varotsis@gmail.com>
2014-01-23 23:59:55 +0000

    added discount_is function

--- discount_calculator_tests.py
+++ discount_calculator_tests.py
@@ -1,1 +1,12 @@
 __author__ = 'Andreas'
+
+def discount_is(total, discount_amount, discount_type):
+    calc_discount = 0
+    if discount_type == 'dollar':
+        calc_discount = discount_amount
+        return calc_discount
+    elif discount_type == 'percent':
+        calc_discount = total/100*discount_amount
+        return calc_discount
+    else:
+        main(total, discount_amount, raw_input("Please enter discount type as percent or dollar"))
